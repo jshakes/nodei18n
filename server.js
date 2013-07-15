a6fbc57ca9b19c5e77bcb77fe3a84e510bac91e7
James Shakespeare <james@bighuman.com>
2013-07-15 19:16:13 -0400

    making a thing

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+// Start the app by listening on <port>
+var port = 3000
+var app = express();
+
+app.listen(port)
+console.log('Express app started on port '+port)
