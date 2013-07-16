588192394cfc17cfe3ab216404e9f301dd27c26f
James Shakespeare <j@jshakespeare.com>
2013-07-15 23:02:02 -0400

    pseudo code

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*
+
+for each file in template src directory:
+
+  Parse file with EJS and i18n
+  List missing translations
+  Create file from the active file (remove .ejs from the filename)
+  Write output to that file
+
+*/
+
 var express = require('express');
 
 // Start the app by listening on <port>
@@ -7,24 +18,27 @@ var app = express();
 app.listen(port)
 console.log('Express app started on port '+port)
 
-var i18n = require("i18n");
-var ejs = require("ejs");
-var fs = require('fs');
+var init = (function(){
+  
+  var i18n = require("i18n");
+  var ejs = require("ejs");
+  var fs = require('fs');
 
-i18n.configure({
-  locales:['en', 'de'],
-  directory: 'locales',
-  defaultLocale: 'en',
-});
+  i18n.configure({
+    locales:['en', 'de'],
+    directory: 'locales',
+    defaultLocale: 'en',
+  });
 
-t = function(key){
+  t = function(key){
 
-  i18n.setLocale('de');
-  return i18n.__(key)
-}
+    i18n.setLocale('de');
+    return i18n.__(key)
+  }
 
-file = fs.readFileSync("template.mustache.ejs", 'utf8');
+  file = fs.readFileSync("template.mustache.ejs", 'utf8');
 
-html = ejs.render(file);
+  html = ejs.render(file);
 
-console.log(html);
+  console.log(html);
+})();
